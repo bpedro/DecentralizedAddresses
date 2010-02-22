2bd2215a32a3426bad0c121067c9483d95fe0362
Sergio Veiga <sergioveiga@gmail.com>
2010-02-22 18:09:47 +0000

    -> added filter_cache

--- filter.js
+++ filter.js
@@ -9,21 +9,32 @@ function findAncestor(el, name)
     else return false;
 }
 
-function filter()
+function filter_cache()
 {
-    var xPathResult = document.evaluate('.//text()[normalize-space(.)!=""]',
-    document.body,
-    null,
-    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-    null);
-    for (var i=0, l=xPathResult.snapshotLength; i<l; i++) {
-        var textNode = xPathResult.snapshotItem(i);
-        if (textNode.data.match(/([^A-Za-z0-9_]|^)@([A-Za-z0-9_]+)/g) &&
-            !findAncestor(textNode,'A')) {
-            textNode.parentNode.innerHTML = textNode.parentNode.innerHTML.replace(/([^A-Za-z0-9_]|^)@([A-Za-z0-9_]+)/g, '$1@<a href="http://twitter.com/$2">$2</a>');
-        }
+    if(tmp != document.body.innerHTML)
+    {
+        console.log("new_stuff");
+        tmp = document.body.innerHTML;
+        filter(); 
     }
 }
 
+function filter()
+{
+        var xPathResult = document.evaluate('.//text()[normalize-space(.)!=""]',
+        document.body,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null);
+        for (var i=0, l=xPathResult.snapshotLength; i<l; i++) {
+            var textNode = xPathResult.snapshotItem(i);
+            
+            if (textNode.data.match(/([^A-Za-z0-9_]|^)@([A-Za-z0-9_]+)/g) &&
+                !findAncestor(textNode,'A')) {
+                textNode.parentNode.innerHTML = textNode.parentNode.innerHTML.replace(/([^A-Za-z0-9_]|^)@([A-Za-z0-9_]+)/g, '$1@<a href="http://twitter.com/$2">$2</a>');
+            }
+        }
+}
 filter();
-setInterval('filter()', 5000);
+var tmp = document.body.innerHTML;
+setInterval('filter_cache()', 5000);
