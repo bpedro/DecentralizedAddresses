863472250c0ee4ec6d52e461babab381df87e889
Bruno Pedro <bpedro@tarpipe.com>
2010-02-23 11:05:22 +0000

    Removed the locally created Timer object.

--- filter.js
+++ filter.js
@@ -1,20 +1,3 @@
-Time = function(){
-    this.start()
-};
-
-Time.prototype = {
-    start : function()
-    {
-        var date1 = new Date(); 
-        this.start_mili = date1.getTime();
-    },
-    end : function(){
-        var date2 = new Date(); 
-        var milliseconds2 = date2.getTime(); 
-        return milliseconds2 - this.start_mili;
-    }
-}
-
 function findAncestor(el, name)
 {
     if (el.nodeName==name) {
